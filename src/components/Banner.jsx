12df60b93afcd1e8c8471bf616b7a8572f619bd8
Rodrigo Manriquez <67527729+Romanriquez25@users.noreply.github.com>
2024-06-12 19:11:53 -0400

    se agrega nav a logeo

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 const Banner = () => {
     return (
         <div className="banner">
-           
+            <div className="bannerComandos">
+                
+                
+            </div>
         </div>
     );
 }
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,11 +2,13 @@
 import Banner from "../components/Banner";
 import FormularioLg from "../components/FormularioLg";
 import Footer from "../components/Footer";
+import Nav from "../components/Nav";
 
 const Login = () => {
     return (
         <>
         <Banner />
+        <Nav />
         <FormularioLg />
         <Footer />
         </>
